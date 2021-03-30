d0f045b3c89fe1ae65128d44afc1836ab20f3e1b
Randeep Singh <rand-singh@users.noreply.github.com>
2021-03-30 22:32:59 +0100

    Remove OOB files. Installed node-sass. Add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQP8ZwXKjlj2XhB64B8eEJbFB2O6wERHU",
+    authDomain: "react-facebook-clone-9b50a.firebaseapp.com",
+    projectId: "react-facebook-clone-9b50a",
+    storageBucket: "react-facebook-clone-9b50a.appspot.com",
+    messagingSenderId: "793482965739",
+    appId: "1:793482965739:web:ed6d5df4274134379b04ae"
+  };
