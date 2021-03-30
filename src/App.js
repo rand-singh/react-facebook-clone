b18d89db3439e22b8068ea669ba8c47ba1e5a982
Randeep Singh <rand-singh@users.noreply.github.com>
2021-03-30 22:42:07 +0100

    Rename css files to scss, create header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <h1>Facebook Clone</h1>
+
+      {/* Header */}
+
+      {/* App Body */}
+        {/* Side bar */}
+        {/* Feed */}
+        {/* Widgets - facebook embed iframe */}
+
     </div>
   );
 }
