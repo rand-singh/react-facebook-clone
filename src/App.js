b716d61fd2da9c8e44727062c9af3876bd65ff00
Randeep Singh <rand-singh@users.noreply.github.com>
2021-04-05 20:17:04 +0100

    Setup sidebar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.scss';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
 	return (
     	<div className='app'>
 			<Header />
 
-			{/* App Body */}
-				{/* Side bar */}
+			<div className="app__body">
+				<Sidebar />
 				{/* Feed */}
 				{/* Widgets - facebook embed iframe */}
+			</div>
+
     	</div>
   	);
 }
